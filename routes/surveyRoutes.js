eeecad7adbb451888660e5046665127097d79caa
Patrick Sheriddan <sheridp2@gmail.com>
2018-12-30 18:48:12 -0800

    email template boilerplate added, mailer service created and tested, email successfully sent

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,20 +1,26 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
-const requireCredts = require('../middlewares/requireCredits');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const requireCredts = require("../middlewares/requireCredits");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
-const Survey = mongoose.model('surveys');
+const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredts, (req, res)=>{
+
+  app.post("/api/surveys", requireLogin, requireCredts, (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
       title,
       subject,
       body,
-      recipients: recipients.split(',').map(email => ({ email:email.trim() })),
+      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now()
-    })
+    });
+
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    mailer.send();
   });
 };
---
+++ services/emailTemplates/surveyTemplate.js
@@ -0,0 +1,9 @@
+const keys = require('../../config/keys');
+
+module.exports = survey => {
+  return `
+    <div>
+      ${survey.body}
+    </div>
+  `;
+};
