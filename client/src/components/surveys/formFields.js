1d7ba7c2c03bab605fcf987a1e5864acaf1c7d2b
Patrick Sheriddan <sheridp2@gmail.com>
2019-01-04 18:40:35 -0800

    submitting from preview now redirects back to dashboard via withRoute helper

--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -2,5 +2,5 @@ export default [
   {label: 'Survey Title', name: 'title', noValueError:'You must provide a survey title'},
   {label: 'Subject Line', name: 'subject', noValueError:'You must provide a subject for your emails'},
   {label: 'Email Body', name: 'body', noValueError:'You must provide a body for the email'},
-  {label: 'Recipient List', name: 'emails', noValueError:'You must provide emails addresses seperated by commas'}
+  {label: 'Recipient List', name: 'recipients', noValueError:'You must provide email addresses seperated by commas'}
 ];
