5258c59390f37ce66c45e14fae77321862472d0c
Patrick Sheriddan <sheridp2@gmail.com>
2019-01-04 18:06:15 -0800

    form inputs now dumped on cancel click

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
-import SurveyForm from "./SurveyForm";
-import SurveyFormReview from "./SurveyFormReview";
+import React, { Component } from 'react';
+import { reduxForm} from 'redux-form';
+import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
   state = { showFormReview: false };
@@ -23,4 +24,6 @@ class SurveyNew extends Component {
   }
 }
 
-export default SurveyNew;
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyNew);
